1b848a9ea28f5c1a1725700855febc9361d69649
JW <leejuwon9282@gmail.com>
2025-05-14 14:14:20 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("💖 Group3 project")
-st.caption("This is a group project of G0")
+st.title("💖 Group3")
+st.caption("Loading...")
 
 st.write("This should be a collaboratory work")
 st.markdown("---")
