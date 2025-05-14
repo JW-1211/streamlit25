1a5a76e0e7fe9e0525b012ea30ea657f0a2e76ff
JW <leejuwon9282@gmail.com>
2025-05-14 14:38:04 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.rice("This app is under construction.")
 
-url="https://github.com/MK316/streamlit25/raw/main/images/image00.png"
+url="https://raw.githubusercontent.com/JW-1211/streamlit25/main/C70CD646-C046-47B1-A76B-B0B51B3E0889.png"
 st.image(url, caption="Image link", width=300)  # width in pixels
 
